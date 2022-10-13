7d07b3fe9c6803198002923e8a66ce4574885cbc
Leonel <leonel-nascimento@hotmail.com>
2022-10-13 05:22:20 +0100

    Perfect form carousel

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const carousel = document.querySelector('.carousel');
 const slides = carousel.querySelector('.slides-container');
 
+let carouselCurrentWidth;
+let currentItemIndex = 0;
+
 // #region Carousel controls
 
 const previousButton = document.querySelector('.previous');
@@ -9,60 +12,53 @@ const nextButton = document.querySelector('.next');
 const trackers = document.querySelector('.slide-trackers');
 // #endregion
 
+
 document.addEventListener('DOMContentLoaded', e => {
-    for(let i = 0; i < slides.childElementCount; i++){
+    for(const _ of slides.children){
         const newTracker = document.createElement('div');
         newTracker.classList.add('tracker');
         trackers.appendChild(newTracker);
     }
     trackers.firstElementChild.classList.add('active');
 })
-
-let carouselCurrentWidth;
-let currentItemIndex = 0;
   
 trackers.addEventListener('click', e => {
-    const { target } = e;
-    if(target.classList.contains('tracker') && !target.classList.contains('active')){
-        clearTrackers();
+    if(e.target.classList.contains('tracker') && !e.target.classList.contains('active')){
 
-        currentItemIndex = Array.from(trackers.children).indexOf(target);
-        
-        slides.scrollTo({left: carouselCurrentWidth * currentItemIndex, behavior: 'smooth'});
-        trackers.children[currentItemIndex].classList.add('active');
-
-        target.classList.add('active');
+        currentItemIndex = Array.from(trackers.children).indexOf(e.target);
+        moveToSlide(carouselCurrentWidth * currentItemIndex);
     }
 })
-
 
 const observer = new ResizeObserver(entries => {
     entries.forEach(entry => {
         carouselCurrentWidth = entry.contentRect.width;
 
-        slides.scrollTo({left: carouselCurrentWidth * currentItemIndex, behavior: 'auto'});
+        slides.classList.add('no-transition');
+        moveToSlide(carouselCurrentWidth * currentItemIndex);
+        setTimeout(() => {
+            slides.classList.remove('no-transition');
+        }, 50);
     })
 });
 observer.observe(carousel);
 
 previousButton.addEventListener('click', e => {
     if(currentItemIndex > 0){
-        slides.scrollTo({left: (carouselCurrentWidth * currentItemIndex--) - carouselCurrentWidth, behavior: 'smooth'});
-        clearTrackers();
-        trackers.children[currentItemIndex].classList.add('active');
+        // slides.scrollTo({left: (carouselCurrentWidth * currentItemIndex--) - carouselCurrentWidth, behavior: 'smooth'});
+        moveToSlide((carouselCurrentWidth * currentItemIndex--) - carouselCurrentWidth);
     }
 })
 
 nextButton.addEventListener('click', e => {
     if(currentItemIndex + 1 < slides.childElementCount){
-        slides.scrollTo({left: carouselCurrentWidth * ++currentItemIndex, behavior: 'smooth'});
-        clearTrackers();
-        trackers.children[currentItemIndex].classList.add('active');
+        // slides.scrollTo({left: carouselCurrentWidth * ++currentItemIndex, behavior: 'smooth'});
+        moveToSlide(carouselCurrentWidth * ++currentItemIndex);
     }
 })
 
-function clearTrackers(){
-    for(const tracker of trackers.children){
-        tracker.classList.contains('active') && tracker.classList.remove('active');
-    };
+function moveToSlide(amountToMove){
+    slides.style.transform = `translateX(-${amountToMove}px)`;
+    trackers.querySelector('.active')?.classList.remove('active');
+    trackers.children[currentItemIndex]?.classList.add('active');
 }
